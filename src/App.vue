c12cb4eeae9c30ff74b4ee04fd50fce77dfb0484
Maksim Hrynevich <maxim.grinevich@yahoo.com>
2023-05-18 14:58:20 +0200

    main navigation panel added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-  <nav>
+    <TheHeader></TheHeader>
     Test
-  </nav>
-  <router-view/>
+    <router-view/>
 </template>
 
-<style>
+<script>
+import TheHeader from "@/components/layout/TheHeader.vue";
 
+export default {
+    name: "App",
+    components: {
+        TheHeader,
+    }
+}
+
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito:wght@300;700&display=swap");
+html {
+    font-family: "Nunito", sans-serif;
+    font-weight: 300;
+}
+
+body, html {
+    margin: 0;
+    padding: 0;
+}
 </style>
--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,13 +1,94 @@
 <template>
+    <header>
+        <div class="container">
+            <img src="src/assets/logo.png" alt="logo" class="logo">
 
+            <nav>
+                <ul>
+                    <li><a href="#">Booking</a></li>
+                    <li><a href="#">General</a></li>
+                    <li><a href="#">Entertainment</a></li>
+                    <li><a href="#">Restaurant</a></li>
+                    <li><a href="#">How it works</a></li>
+                </ul>
+            </nav>
+
+            <div class="login-btn">
+                <button>Login</button>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
-export default {
-    name: "TheHeader"
-}
+
 </script>
 
 <style scoped>
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    background: #1F232C;
+}
+
+.logo {
+    height: 50px;
+}
+
+nav ul {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    gap: 60px;
+}
+
+nav ul li a {
+    text-decoration: none;
+    color: white;
+    font-size: 17px;
+}
+
+nav ul li a:hover {
+    color: #FC5C65;
+}
+
+.login-btn button {
+    padding: 7px 30px;
+    border: none;
+    background-color: #FC5C65;
+    color: #1F232C;
+    font-size: 18px;
+    cursor: pointer;
+    border-radius: 20px;
+}
+
+.login-btn button:hover {
+    background-color: #e33840;
+}
+
+@media (max-width: 600px) {
+    .container {
+        flex-direction: column;
+    }
+
+    nav ul {
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .login-btn {
+        margin-top: 20px;
+    }
+}
 
 </style>
